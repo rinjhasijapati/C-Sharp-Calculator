a90584a909d8adc1480f25796371ba105f64dc6d
rinjhasijapati <aastha_sijapati@yahoo.com>
2022-11-23 09:19:52 +0545

    first commit

---
+++ calculator/calculator/Form1.cs
@@ -0,0 +1,134 @@
+namespace calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = String.Empty;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "7";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "8";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "9";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "4";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "5";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "6";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "1";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "2";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "3";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "0";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + ".";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data.Remove(data.Length - 1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "%";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "x";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "/";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "-";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "+";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            textBox1.Text = data + "( )";
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "=";
+        }
+    }
+}
